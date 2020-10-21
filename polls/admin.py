701fcf6ed3e3f0db82bfbfb4b983012a71deb580
Your Name <you@example.com>
2020-10-21 12:10:34 +0000

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from polls.models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+
+
+from django.contrib import admin
+from polls.models import Choice, Question
+# ...
+admin.site.register(Choice)
